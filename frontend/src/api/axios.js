ea1e7ec5c46afe77d36b960d100eb4b04519e4a8
MusthafaPm-135 <editsmania3@gmail.com>
2026-07-29 19:43:57 +0530

    Add credentials to axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,7 +6,7 @@ export const API_ORIGIN = 'https://inkburst-backend.onrender.com';
 // 2. Setup the Axios instance for your data fetching
 const API = axios.create({
   baseURL: `${API_ORIGIN}/api`, 
+  withCredentials: true // <-- This tells React to send your login cookie!
 });
-
 // 3. Export the default instance
 export default API;
