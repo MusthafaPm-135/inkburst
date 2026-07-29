91ea20e4a2c1aa42102d8e2bb7451891d130dab6
MusthafaPm-135 <editsmania3@gmail.com>
2026-07-29 20:30:39 +0530

    Fix token passing in comic fetch requests

--- frontend/src/api/comics.js
+++ frontend/src/api/comics.js
@@ -1,13 +1,12 @@
-import axios from "axios";
-
-const API = axios.create({
-    baseURL: "https://inkburst-backend.onrender.com/api",
-    withCredentials: true
-});
+import API from "./axios"; // Import your custom API instance that has the token interceptor
 
 export const getComics = async () => {
-    const res = await API.get("/comics");
-    return res.data.comics;
+    try {
+        const response = await API.get("/comics"); // Use API.get instead of axios.get
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
 };
 
 export default API;
